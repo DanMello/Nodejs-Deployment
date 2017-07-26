3a6fe0734a6c073148573faf115d396068e2570a
Dan Mello <jdanmello@gmail.com>
2017-07-26 17:55:55 -0400

    create an nginx server to run as a load balancer for both my servers aka round robin

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,10 +19,12 @@ module.exports = {
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
+
+  // I setup a third server on vmware 172.16.60.128 with nginx running as a load balancer for these servers 
   deploy : {
     production : {
       user : 'deploy', 
-      host : ['172.16.60.129', '172.16.60.131'],  // This is a local vmware server with an ssh authorized key and no password
+      host : ['172.16.60.129', '172.16.60.131'],  // These are local vmware servers with an ssh authorized key and no password
       ref  : 'origin/master',
       repo : 'git@github.com:DanMello/first-nodejs-app-deployment.git',
       path : '/home/deploy/web/nodejs-app-production', 
@@ -30,3 +32,4 @@ module.exports = {
     }
   }
 };
+
