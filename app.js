eda293a768ab456a7f5669d077bd19aae38e7f2f
Dan Mello <jdanmello@gmail.com>
2017-07-26 19:24:44 -0400

    added consolelog to test nginx

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express =  require("express")
 
 express()
     .get("/", (req, res, next) => {
+      console.log("request received", Date.now())
       res.send("hello man")
     })
     .listen(3000)
